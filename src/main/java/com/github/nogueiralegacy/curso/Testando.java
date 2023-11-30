bfce8aeab2d31985a9da07d7962740f7e2fdae01
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-11-30 19:08:00 -0300

    start

---
+++ src/main/java/com/github/nogueiralegacy/curso/Testando.java
@@ -0,0 +1,18 @@
+package com.github.nogueiralegacy.curso;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/sua-rota")
+public class Testando extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println("{ \"mensagem\": \"Olá do servidor Java!\" }");
+    }
+}
